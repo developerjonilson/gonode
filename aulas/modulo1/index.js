3dde31ed34cf80b743afdb09aedc03d9e96f1b7f
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-02-20 22:11:19 -0300

    final modulo 1

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -1,15 +1,30 @@
-const express = require("express");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  return res.send(`Bem-vindo, ${req.query.name}`);
-});
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
 
-app.get("/nome/:name", (req, res) => {
-  return res.json({
-    message: `Welcome, ${req.params.name}`
-  });
-});
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
-app.listen(3000);
+const users = ['Jonilson Sousa', 'Diego Fernandes', 'Cleiton Souza']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(3000)
