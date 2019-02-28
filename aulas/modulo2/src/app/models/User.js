2a154e3562db329e2fc37074304a628b1ad4682a
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-02-28 20:54:30 -0300

    comencando agendamento

--- aulas/modulo2/src/app/models/User.js
+++ aulas/modulo2/src/app/models/User.js
@@ -1,22 +1,30 @@
 const bycript = require('bcryptjs')
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
-    password_hash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN
-  }, {
-    hooks: {
-      beforeSave: async user => {
-        if (user.password) {
-          user.password_hash = await bycript.hash(user.password, 8)
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bycript.hash(user.password, 8)
+          }
         }
       }
-    },
-  })
+    }
+  )
+
+  User.prototype.checkPassword = function (password) {
+    return bycript.compare(password, this.password_hash)
+  }
 
   return User
 }
