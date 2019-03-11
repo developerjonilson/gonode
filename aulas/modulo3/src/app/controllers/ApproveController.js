496b30bbe53fac9fb3cfa0bc8154a4c55014fdde
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-11 16:13:37 -0300

    completo

--- aulas/modulo3/src/app/controllers/ApproveController.js
+++ aulas/modulo3/src/app/controllers/ApproveController.js
@@ -4,15 +4,6 @@ class ApproveController {
   async update (req, res) {
     const { id } = req.params
 
-    // const { ad } = await Purchase.findById({ id }).populate({
-    //   path: 'ad',
-    //   populate: {
-    //     path: 'author'
-    //   }
-    // })
-
-    console.log(id)
-
     const { ad } = await Purchase.findById(id).populate({
       path: 'ad',
       populate: {
@@ -20,19 +11,19 @@ class ApproveController {
       }
     })
 
-    // if (!ad.author._id.equals(req.userId)) {
-    //   return res.status(401).json({ error: "You're not the ad author" })
-    // }
+    if (!ad.author._id.equals(req.userId)) {
+      return res.status(401).json({ error: "You're not the ad author" })
+    }
 
-    // if (ad.purchaseBy) {
-    //   return res
-    //     .status(400)
-    //     .json({ error: 'This ad had already been purchased' })
-    // }
+    if (ad.purchasedBy) {
+      return res
+        .status(400)
+        .json({ error: 'This ad had already been purchased' })
+    }
 
-    // ad.purchaseBy = id
+    ad.purchasedBy = id
 
-    // await ad.save()
+    await ad.save()
 
     return res.json(ad)
   }
