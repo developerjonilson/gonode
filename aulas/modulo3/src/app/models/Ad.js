790f42a4cc2e8e7991264b638c936938363b4ce1
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-10 22:32:56 -0300

    fazer desafio

---
+++ aulas/modulo3/src/app/models/Ad.js
@@ -0,0 +1,30 @@
+const moongose = require('mongoose')
+const moongosePaginate = require('mongoose-paginate')
+
+const Ad = new moongose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+Ad.plugin(moongosePaginate)
+
+module.exports = moongose.model('Ad', Ad)
